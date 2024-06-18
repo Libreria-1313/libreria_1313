0b40b65af1007bd83c2a3bf4bf2d8a6ea5c880ba
SxntiagoLara <daniel.lara07c@gmail.com>
2024-06-18 15:38:52 -0500

    grupo 1

--- js/app10.js
+++ js/app10.js
@@ -2,9 +2,40 @@
 
 // Lista de libros inicial
 let books = [
-    { title: "Libro 1", editorial: "Editorial A", stock: 10 },
-    { title: "Libro 2", editorial: "Editorial B", stock: 5 },
-    { title: "Libro 3", editorial: "Editorial C", stock: 2 }
+    {
+        Image: "../assets/img/img1.jpeg",
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        genero: "Ficción",
+        idioma: "Español",
+        precio: "15.99 EUR",
+        formato: "Físico",
+        isbn: "978-3-16-148410-0",
+        descripcion: "Una de las obras más importantes de la literatura hispanoamericana.",
+        estado: "Nuevo",
+        ubicacion: "Madrid, España",
+        fecha_publicacion: "1967-06-05",
+        editorial: "Editorial Sudamericana",
+        paginas: 471,
+        stock: 10
+        },
+        {
+        Image: "../assets/img/El nombre de la rosa.jpg",
+        titulo: "El nombre de la rosa",
+        autor: "Umberto Eco",
+        genero: "Misterio",
+        idioma: "Italiano",
+        precio: "12.50 EUR",
+        formato: "Físico",
+        isbn: "978-84-339-1162-4",
+        descripcion: "Una novela histórica y de misterio ambientada en una abadía italiana medieval.",
+        estado: "Usado",
+        ubicacion: "Roma, Italia",
+        fecha_publicacion: "1980-01-01",
+        editorial: "Bompiani",
+        paginas: 592,
+        stock: 15
+        }
 ];
 
 // Función para cargar el listado de libros en la página
@@ -15,7 +46,7 @@ function loadBooks() {
     books.forEach((book, index) => {
         // Añadir a la lista de libros
         const listItem = document.createElement('li');
-        listItem.textContent = `${book.title} - Editorial: ${book.editorial} - Stock: ${book.stock}`;
+        listItem.textContent = `${book.titulo} - Editorial: ${book.editorial} - Stock: ${book.stock}`;
         bookList.appendChild(listItem);
     });
 }
@@ -30,7 +61,7 @@ function searchAndDeleteBook() {
     for (let i = 0; i < books.length; i++) {
         const book = books[i];
         if (
-            (searchCriteria === 'title' && book.title.toLowerCase() === searchValue) ||
+            (searchCriteria === 'title' && book.titulo.toLowerCase() === searchValue) ||
             (searchCriteria === 'editorial' && book.editorial.toLowerCase() === searchValue) ||
             (searchCriteria === 'stock' && book.stock.toString() === searchValue)
         ) {
--- js/app5.js
+++ js/app5.js
@@ -1,3 +1,129 @@
+let books = [
+    {
+        Image: "../assets/img/img1.jpeg",
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        genero: "Ficción",
+        idioma: "Español",
+        precio: "15.99 EUR",
+        formato: "Físico",
+        isbn: "978-3-16-148410-0",
+        descripcion: "Una de las obras más importantes de la literatura hispanoamericana.",
+        estado: "Nuevo",
+        ubicacion: "Madrid, España",
+        fecha_publicacion: "1967-06-05",
+        editorial: "Editorial Sudamericana",
+        paginas: 471,
+        stock: 10
+        },
+        {
+        Image: "../assets/img/El nombre de la rosa.jpg",
+        titulo: "El nombre de la rosa",
+        autor: "Umberto Eco",
+        genero: "Misterio",
+        idioma: "Italiano",
+        precio: "12.50 EUR",
+        formato: "Físico",
+        isbn: "978-84-339-1162-4",
+        descripcion: "Una novela histórica y de misterio ambientada en una abadía italiana medieval.",
+        estado: "Usado",
+        ubicacion: "Roma, Italia",
+        fecha_publicacion: "1980-01-01",
+        editorial: "Bompiani",
+        paginas: 592,
+        stock: 15
+        }
+];
+
+
+function loadBooks() {
+    const bookSelectStock = document.getElementById('bookSelectStock');
+    bookSelectStock.innerHTML = ''; // Limpiar opciones anteriores
+
+    books.forEach((book, index) => {
+        const option = document.createElement('option');
+        option.value = index;
+        option.textContent = book.titulo;
+        bookSelectStock.appendChild(option);
+    });
+
+    // Llamar a la función para mostrar los libros
+    showBooks();
+}
+
+function showBooks() {
+    const divbooks = document.getElementById('divbooks');
+    divbooks.innerHTML = '';
+
+    books.forEach(book => {
+        const myBook = document.createElement('div');
+        myBook.classList.add('book');
+
+        const title = document.createElement('h2');
+        title.textContent = book.titulo;
+
+        const image = document.createElement('img');
+        image.src = book.Image;
+        image.alt = book.titulo;
+
+        const autor = document.createElement('p');
+        autor.textContent = `Autor: ${book.autor}`;
+
+        const genero = document.createElement('p');
+        genero.textContent = `Género: ${book.genero}`;
+
+        const idioma = document.createElement('p');
+        idioma.textContent = `Idioma: ${book.idioma}`;
+
+        const precio = document.createElement('p');
+        precio.textContent = `Precio: ${book.precio}`;
+
+        const formato = document.createElement('p');
+        formato.textContent = `Formato: ${book.formato}`;
+
+        const isbn = document.createElement('p');
+        isbn.textContent = `ISBN: ${book.isbn}`;
+
+        const descripcion = document.createElement('p');
+        descripcion.textContent = `Descripción: ${book.descripcion}`;
+
+        const estado = document.createElement('p');
+        estado.textContent = `Estado: ${book.estado}`;
+
+        const ubicacion = document.createElement('p');
+        ubicacion.textContent = `Ubicación: ${book.ubicacion}`;
+
+        const fecha = document.createElement('p');
+        fecha.textContent = `Fecha de publicación: ${book.fecha_publicacion}`;
+
+        const editorial = document.createElement('p');
+        editorial.textContent = `Editorial: ${book.editorial}`;
+
+        const paginas = document.createElement('p');
+        paginas.textContent = `Páginas: ${book.paginas}`;
+
+        const stock = document.createElement('p');
+        stock.textContent = `Stock: ${book.stock}`;
+
+        myBook.appendChild(title);
+        myBook.appendChild(image);
+        myBook.appendChild(autor);
+        myBook.appendChild(genero);
+        myBook.appendChild(idioma);
+        myBook.appendChild(precio);
+        myBook.appendChild(formato);
+        myBook.appendChild(isbn);
+        myBook.appendChild(descripcion);
+        myBook.appendChild(estado);
+        myBook.appendChild(ubicacion);
+        myBook.appendChild(fecha);
+        myBook.appendChild(editorial);
+        myBook.appendChild(paginas);
+        myBook.appendChild(stock);
+
+        divbooks.appendChild(myBook);
+    });
+}
 
 // Función para ajustar el stock de un libro seleccionado
 function adjustStock(increase) {
@@ -11,7 +137,7 @@ function adjustStock(increase) {
         } else {
             books[selectedIndex].stock = Math.max(0, books[selectedIndex].stock - stockChange);
         }
-        loadBooks();
+        showBooks(); // Actualizar la visualización del stock
         alert('Stock ajustado con éxito');
     } else {
         alert('Por favor, seleccione un libro y complete el campo de cantidad a ajustar.');
